24b8864faff06a8156a2d745f57a31f2f6c3f179
mtalhakhan869 <talhakhan153267@gmail.com>
2024-06-24 07:31:23 -0700

    1st web api

---
+++ book store/book store/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace book_store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values/talha/{id}
+        [HttpGet("talha/{id}")]
+        public IActionResult Get(int id)
+        {
+            if (id > 5)
+
+            {
+                return Ok(1); // Return HTTP 200 OK with the value 1
+            }
+            else
+                return NotFound();
+        }
+    }
+}
